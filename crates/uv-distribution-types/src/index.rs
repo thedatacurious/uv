062b71d5f6f22ef17bc755265c1108690675606c
John Mumm <jtfmumm@gmail.com>
2025-03-07 17:49:24 +0100

    ..

--- crates/uv-distribution-types/src/index.rs
+++ crates/uv-distribution-types/src/index.rs
@@ -128,7 +128,7 @@ impl Index {
             default: true,
             origin: None,
             publish_url: None,
-            authentication: AuthPolicy::Auto,
+            authentication: AuthPolicy::default(),
         }
     }
 
@@ -141,7 +141,7 @@ impl Index {
             default: false,
             origin: None,
             publish_url: None,
-            authentication: AuthPolicy::Auto,
+            authentication: AuthPolicy::default(),
         }
     }
 
@@ -154,7 +154,7 @@ impl Index {
             default: false,
             origin: None,
             publish_url: None,
-            authentication: AuthPolicy::Auto,
+            authentication: AuthPolicy::default(),
         }
     }
 
@@ -228,7 +228,7 @@ impl FromStr for Index {
                     default: false,
                     origin: None,
                     publish_url: None,
-                    authentication: AuthPolicy::Auto,
+                    authentication: AuthPolicy::default(),
                 });
             }
         }
@@ -242,7 +242,7 @@ impl FromStr for Index {
             default: false,
             origin: None,
             publish_url: None,
-            authentication: AuthPolicy::Auto,
+            authentication: AuthPolicy::default(),
         })
     }
 }
